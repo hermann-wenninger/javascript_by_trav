7ee521e3bcd94bb5c8364e4217ee209d2ce80a3d
hermann-wenninger <hermann23@vivaldi.net>
2023-02-03 15:20:02 +0100

    pro3

--- sec2/app.js
+++ sec2/app.js
@@ -1,6 +1,9 @@
 const a = document.getElementById('task-title')
 console.log(a)
-const lis = document.getElementsByTagName('li')
+let lis = document.getElementsByTagName('li')
 console.log(lis)
 lis[3].style.color = 'red'
 lis[3].textContent = 'fragwürdig'
+
+lis = Array.from(lis);
+lis.forEach((li)=>li.style.color ='green')
