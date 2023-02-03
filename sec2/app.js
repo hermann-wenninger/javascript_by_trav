53dbc70222a90a7202213b8c6d48a643a46696bc
hermann-wenninger <hermann23@vivaldi.net>
2023-02-03 15:16:55 +0100

    pro3

--- sec2/app.js
+++ sec2/app.js
@@ -1,1 +1,6 @@
-console.log(document.all)
+const a = document.getElementById('task-title')
+console.log(a)
+const lis = document.getElementsByTagName('li')
+console.log(lis)
+lis[3].style.color = 'red'
+lis[3].textContent = 'fragwürdig'
