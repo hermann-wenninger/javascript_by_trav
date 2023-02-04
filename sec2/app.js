3aad54c9d6979457405cae93a737e0d602d30442
hermann-wenninger <hermann23@vivaldi.net>
2023-02-04 18:29:45 +0100

    around the dome

--- sec2/app.js
+++ sec2/app.js
@@ -6,4 +6,46 @@ lis[3].style.color = 'red'
 lis[3].textContent = 'fragwürdig'
 
 lis = Array.from(lis);
-lis.forEach((li)=>li.style.color ='green')
+lis.forEach((li, index)=>{
+    li.style.color ='green';
+    li.textContent=`${index} Fritzens Fritz`;
+    lis.reverse()
+    lis[3].textContent= 'ASDFGHZUJIKLO'
+})
+
+//const i = document.querySelectorAll('ul.collection li.collection-item')
+//console.log(i)
+
+let v;
+const l = document.querySelector('ul.collection');
+const lit = document.querySelector('li.collection-item:first-child');
+k = l.children
+console.log('k',k);
+
+//1- element
+//2- attribute
+//3- text node
+//8- commet
+//9- document itself
+//10- doctype
+
+k[0].textContent = 'alabaster'
+k = l.firstElementChild;
+console.log(k.style.color='red')
+
+/*
+list.firstChild
+list.firstElementChild
+
+list.lastChild
+list.lastElementChild
+*/
+
+//count child elements
+console.log(l.childElementCount)
+
+console.log(l.parentNode.parentNode.parentNode)
+//parentElementNode
+console.log(l.nextSibling.nextSibling.parentNode.parentElement.attributes)
+console.log(l.nextElementSibling)
+
